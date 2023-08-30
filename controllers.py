77fe08ff70ec15ae442745b0bee35a3bd717cc54
himalayo <himalayo@protonmail.com>
2023-08-30 03:37:49 +0100

    Add feedback to MPC

--- controllers.py
+++ controllers.py
@@ -17,8 +17,8 @@ class MPC:
         self.remi = np.zeros([1,180,1])
         self.nnet = nnet
 
-    def update(self):
-        p,r = self.gen_infusion()
+    def update(self,ref,x):
+        p,r = self.gen_infusion(ref,x-self.nnet.predict((self.prop,self.remi,self.patient.z),verbose=None))
         prediction = self.predict(p,r)
         self.prop = np.concatenate((self.prop[0,self.horizon:,0],p)).reshape([1,180,1])
         self.remi = np.concatenate((self.remi[0,self.horizon:,0],r)).reshape([1,180,1])
@@ -29,26 +29,27 @@ class MPC:
         remi_infusion = b(self.remi,r)
         return self.nnet.predict((prop_infusion,remi_infusion,np.tile(self.patient.z,(self.horizon,1))),verbose=None)
 
-    def gen_infusion(self):
-        inputs = scipy.optimize.minimize(self.gen_cost(0.5),np.ones(self.horizon*2),method='nelder-mead',options={'disp':True,'maxfev':self.horizon*100},bounds=[(0,20)],tol=1).x
+    def gen_infusion(self,ref,bias):
+        ref -= bias 
+        inputs = scipy.optimize.minimize(self.gen_cost(ref),np.ones(self.horizon*2),method='nelder-mead',options={'disp':True,'maxfev':self.horizon*100},bounds=[(0,20)],tol=1).x
         return inputs[:self.horizon],inputs[self.horizon:]
 
     def gen_cost(self,ref):
         return lambda x: sum((self.nnet.predict((b(self.prop,x[:self.horizon]),b(self.remi,x[self.horizon:]),np.tile(self.patient.z,(self.horizon,1))),verbose=None)-ref)**2)*10000
-        #return lambda x: sum([((self.nnet.predict((np.concatenate((np.zeros([1,self.horizon-i,1]),self.prop,x[:self.horizon].reshape([1,self.horizon,1])[:,:i,:]),axis=1),np.concatenate((np.zeros([1,self.horizon-i,1]),self.remi,x[self.horizon:].reshape([1,self.horizon,1])[:,:i,:]),axis=1),self.patient.z),verbose=None)[0]-ref)*100)**2 for i in range(self.horizon)])
 
 
 
 def test():
-    p = Patient(50,170,60,Gender.F)
+    p = Patient(30,180,60,Gender.F)
     nnet = load_model('./weights')
     mpc = MPC(p,nnet,5)
-    bis = []
-    for i in range(10):
+    bis = list(nnet.predict((np.zeros([1,180,1]),np.zeros([1,180,1]),p.z)))
+    at = time.time()
+    for i in range(20):
         st = time.time()
-        b = mpc.update()
-        bis.extend(b)
-        print(b,time.time()-st)
-
+        b = mpc.update(0.5,bis[-1])
+        bis.extend(b+0.05)
+        print([x[0] for x in bis[-mpc.horizon:]],time.time()-st)
+    print(time.time()-at)
     plt.figure()
     plt.plot(bis)
