ba7e616025fdaae3a5944a8afe5f362d7c4c90b7
Himalayo <himalayo@protonmail.com>
2024-02-15 18:02:17 +0000

    pid optimization stuff

--- controllers.py
+++ controllers.py
@@ -50,36 +50,28 @@ class Pharmacodynamic:
         return self.hill(p[0,-1,0],r[0,-1,0]) 
 
 @tf.function
-def stateless_pid(k,last_err,err_i,y,ref,rho,prop,remi,pred):
-    err = ref-y
-    d = (err-last_err)
-    i = tf.reshape(err_i+(err*1),())
-    action = tf.clip_by_value(k[0]*(err+(1/k[1])*i+(k[2]/1)*d),0,np.inf)
-    o = pred.mdl((tf.concat((prop[:,1:,:],tf.reshape(action,(1,1,1))),axis=1),tf.concat((remi[:,1:,:],rho*tf.reshape(action,(1,1,1))),axis=1),pred.z))
-    return i,tf.reshape(action,(1,1,1)),o
+def stateless_pid(k,l_e,e_i,y,ref):
+    e = ref-y
+    d = (e-l_e)
+    i = tf.reshape(e_i+(e+l_e)/2.0,())
+    action = tf.tensordot([e,i,d],k,1)
+    return action
 
 class PID:
-    def __init__(self,mdl,p=1,i=1,d=1):
-        self.rho = 1
+    def __init__(self,mdl,p=1,i=1,d=1,rho=1):
+        self.rho =rho 
         self.pred = mdl
         self.k = tf.cast([p,i,d],dtype='float32')
         self.err = tf.Variable(0.5-0.98)
         self.err_i = tf.Variable(0.0)
-        self.prop = tf.TensorArray(tf.float32,size=180,dynamic_size=False)
-        self.remi = tf.TensorArray(tf.float32,size=180,dynamic_size=False)
-        self.prop.unstack(tf.zeros(180))
-        self.remi.unstack(tf.zeros(180))
-        self.index = 0
-
+        self.prop = tf.zeros((1,180,1))
 
     def update(self,ref,y):
-        state = stateless_pid(self.k,self.err,self.err_i,y,ref,self.rho,tf.reshape(self.prop.stack(),(1,180,1)),tf.reshape(self.remi.stack(),(1,180,1)),self.pred)
-        print(state)
-        self.err_i.assign(state[0])
-        self.prop = self.prop.write(self.index,state[1][0][0][0])
-        self.remi = self.remi.write(self.index,state[1][0][0][0]*self.rho)
-        self.index += 1
-        return state[2][0][0] 
+        u = tf.clip_by_value(stateless_pid(self.k,self.err,self.err_i,y,ref),0,40)
+        self.prop = tf.concat((self.prop[:,1:,:],[[[u]]]),axis=1)
+        self.err_i.assign_add(((ref-y)+self.err)/2)
+        self.err.assign(ref-y)
+        return self.pred.mdl((self.prop,self.prop*self.rho,self.pred.patient.z))[-1][-1]
     @tf.function
     def __call__(self,ref,y):
         return self.update(ref,y)
@@ -100,37 +92,37 @@ class MPC:
     def gen_infusion(self,ref,bias):
         ref -= bias 
         maxiter = 1e6
-        loss = lambda x: stateless_cost(self.nnet,self.patient.z,self.prop,self.remi,self.horizon,x,ref)
-        j = lambda x: loss_jac(self.nnet,self.patient.z,self.prop,self.remi,self.horizon,x,ref)
-        h = lambda x: loss_hess(self.nnet,self.patient.z,self.prop,self.remi,self.horizon,x,ref)
+        loss = lambda x: mpc_loss(self.nnet,self.patient.z,self.prop,self.remi,self.horizon,x,ref)
+        j = lambda x: mpc_loss_jac(self.nnet,self.patient.z,self.prop,self.remi,self.horizon,x,ref)
+        h = lambda x: mpc_loss_hess(self.nnet,self.patient.z,self.prop,self.remi,self.horizon,x,ref)
         inputs = scipy.optimize.minimize(loss,tf.ones(self.horizon*2),jac=j,method='L-BFGS-B',options={'disp':False}).x
         return inputs[:self.horizon],inputs[self.horizon:]
 @tf.function
-def loss_hess(nnet,z,prop,remi,horizon,x,ref):
+def mpc_loss_hess(nnet,z,prop,remi,horizon,x,ref):
     x = tf.cast(x,prop.dtype)
     a = 0.0
     for i in range(1,horizon):
         a += ((nnet((tf.reshape(tf.concat((prop[0,i:,0],x[:i]),axis=0),(1,180,1)),tf.reshape(tf.concat((remi[0,i:,0],x[horizon:horizon+i]),axis=0),(1,180,1)),z))[-1][-1]-ref)**2)*(100/(i+1))
-    if not tf.math.reduce_all(tf.abs(x)==x):
-        a *= 1e16     
+    if not tf.math.reduce_all(x>tf.ones(x.shape)*0.05) or not tf.math.reduce_all(x<=tf.ones(x.shape)*40):
+        a *= 1e16      
     return tf.gradients(tf.gradients(a,x)[0],x)[0]
 
 @tf.function
-def loss_jac(nnet,z,prop,remi,horizon,x,ref):
+def mpc_loss_jac(nnet,z,prop,remi,horizon,x,ref):
     x = tf.cast(x,prop.dtype)
     a = 0.0
     for i in range(1,horizon):
-        a += ((nnet((tf.reshape(tf.concat((prop[0,i:,0],x[:i]),axis=0),(1,180,1)),tf.reshape(tf.concat((remi[0,i:,0],x[horizon:horizon+i]),axis=0),(1,180,1)),z))[-1][-1]-ref)**2)*(1000/(i+1))
-    if not tf.math.reduce_all(tf.abs(x)==x):
-        a *= 1e16 
+        a += ((nnet((tf.reshape(tf.concat((prop[0,i:,0],x[:i]),axis=0),(1,180,1)),tf.reshape(tf.concat((remi[0,i:,0],x[horizon:horizon+i]),axis=0),(1,180,1)),z))[-1][-1]-ref)**2)*(100/(i+1))
+    if not tf.math.reduce_all(x>tf.ones(x.shape,x.dtype)*0.05) or not tf.math.reduce_all(x<=tf.ones(x.shape,x.dtype)*40):
+        a *= 1e16     
     return tf.gradients(a,x)[0]
 @tf.function
-def stateless_cost(nnet,z,prop,remi,horizon,x,ref):
+def mpc_loss(nnet,z,prop,remi,horizon,x,ref):
     x = tf.cast(x,prop.dtype)
     a = 0.0
     for i in range(1,horizon):
-        a += ((nnet((tf.reshape(tf.concat((prop[0,i:,0],x[:i]),axis=0),(1,180,1)),tf.reshape(tf.concat((remi[0,i:,0],x[horizon:horizon+i]),axis=0),(1,180,1)),z))[-1][-1]-ref)**2)*(1000/(i+1))
-    if not tf.math.reduce_all(tf.abs(x)==x):
+        a += ((nnet((tf.reshape(tf.concat((prop[0,i:,0],x[:i]),axis=0),(1,180,1)),tf.reshape(tf.concat((remi[0,i:,0],x[horizon:horizon+i]),axis=0),(1,180,1)),z))[-1][-1]-ref)**2)*(100/(i+1))
+    if not tf.math.reduce_all(x>tf.ones(x.shape,x.dtype)*0.05) or not tf.math.reduce_all(x<=tf.ones(x.shape,x.dtype)*40):
         a *= 1e16 
     return a
 
@@ -145,88 +137,71 @@ def lowpass(xs,dt,rc):
 def get_smooth(xs):
     return lowpass(xs,1,3)
 
-def get_PID(mdl,ref):
+def get_PID(pred,ref,x0=tf.constant([-1.55,0,0.4])):
     @tf.function
-    def tot_err(u,ref,n):
+    def gradient(kp,ki,kd,rho):
+        f = loss(kp,ki,kd,rho,tf.constant(0.5),tf.constant(140),pred)
+        grad = tf.gradients(f,[kp,ki,kd])
+        tf.print([kp,ki,kd,rho],f,grad)
+        return grad 
+    return scipy.optimize.minimize(lambda x: tf.cast(pid_loss(*tf.cast(x,tf.float32),tf.constant(1.0),tf.constant(0.5),tf.constant(140),pred),tf.float64),x0,jac=lambda x: tf.cast(gradient(*tf.cast(x,tf.float32),1),tf.float64),method='L-BFGS-B',options={'disp':True}).x
+
+def swarm_PID(pred,ref):
+    @tf.function
+    def cost(y):
+        return tf.vectorized_map(lambda x: pid_loss(x[0],x[1],x[2],1,tf.constant(0.5,tf.float32),140,pred),tf.cast(y,tf.float32))
+    options = {'c1':0.05, 'c2':0.03, 'w':0.09}
+    optimizer = ps.single.GlobalBestPSO(n_particles=700, dimensions=3, options=options)
+    return optimizer.optimize(cost,iters=50)[1]
+
+@tf.function
+def pid_loss(kp,ki,kd,rho,ref,n,pred):
         last_err = 0.5-0.98
         i = 0.0
         y = 0.98
         s = 0.0
-        prop = tf.TensorArray(tf.float32,size=180)
-        prop.unstack(tf.zeros((180)))
-        remi = tf.TensorArray(tf.float32,size=180)
-        remi.unstack(tf.zeros((180)))
+        prop = tf.zeros((1,180,1))
+        remi = tf.zeros((1,180,1))
         for j in range(n):
-            state = stateless_pid(u,last_err,i,y,ref,0.1,tf.reshape(prop.stack(),(1,180,1)),tf.reshape(remi.stack(),(1,180,1)),mdl)
-            prop = prop.write(j,state[1][0][0][0])
-            remi = remi.write(j,state[1][0][0][0]*0.1)
-            last_err = ref-state[2][0][0]
-            y = state[2][0][0]
-            i = state[0]
-            s += tf.abs(last_err)
-        return 1e5*s
-    @tf.function
-    def cost(x):
-        return tot_err(tf.cast(x,tf.float32),tf.cast(ref,tf.float32),tf.constant(90))
-    @tf.function
-    def j(x):
-        a = tf.cast(x,tf.float32)
-        return tf.gradients(tot_err(a,0.5,40),a)
-    print(j(tf.constant([-1,0,0])))
-    return scipy.optimize.minimize(cost,np.array([-1,0,0]),jac=j,options={'maxiter':100,'disp':True}).x
+            err = ref-y
+            i += (err+last_err)/2
+            pu = kp*err+ki*i+kd*(err-last_err)
+            ru = rho*(kp*err+ki*i+kd*(err-last_err))
+            if pu<0:
+                pu = tf.constant(0.0,pu.dtype)
+            if pu>40:
+                pu = tf.constant(40.0,pu.dtype)
+            if ru<0:
+                ru = tf.constant(0.0,ru.dtype)
+            if ru>40:
+                ru = tf.constant(40.0,ru.dtype)
+            last_err = err 
+            prop = tf.concat((prop[:,1:,:],[[[pu]]]),axis=1)
+            remi = tf.concat((remi[:,1:,:],[[[ru]]]),axis=1)
+            y = pred.mdl((prop,remi,pred.patient.z))[-1][-1]
+            s += (last_err*10)**2
+        return s   
 
-def swarm_PID(mdl,ref):
-    @tf.function
-    def tot_err(u,ref,n):
-        last_err = 0.98
-        i = 0.0
-        y = 0.98
-        s = 0.0
-        prop = tf.TensorArray(tf.float32,size=180)
-        prop.unstack(tf.zeros((180)))
-        remi = tf.TensorArray(tf.float32,size=180)
-        remi.unstack(tf.zeros((180)))
-        for j in range(1,n):
-            state = stateless_pid(u,last_err,i,y,ref,2.0,tf.reshape(prop.stack(),(1,180,1)),tf.reshape(remi.stack(),(1,180,1)),mdl)
-            prop = prop.write(j,state[1][0][0][0])
-            remi = remi.write(j,state[1][0][0][0]*2)
-            last_err = ref-state[2][0][0]
-            y = state[2][0][0]
-            i = state[0]
-            s += tf.abs(last_err)
-        return 1e5*s
+def pid_loss_plot(pred,i):
+    p = tf.cast(tf.linspace(-3,3,100),tf.float32)
+    d = tf.cast(tf.linspace(-3,3,100),tf.float32)
+    P,D = tf.meshgrid(p,d)
+
     @tf.function
     def cost(y):
-        return tf.vectorized_map(lambda x: tot_err(tf.reshape([x[0],x[1],x[2]],(3,1)),tf.constant(0.5,tf.float32),tf.constant(70)),tf.cast(y,tf.float32))
-    print(cost(tf.constant([[1.0,1.0,1.0]])))
-    options = {'c1':5, 'c2':3, 'w':9}
-    optimizer = ps.single.GlobalBestPSO(n_particles=5000, dimensions=3, options=options)
-    return optimizer.optimize(cost,iters=50)[1]
-    
-def PID_plot(mdl):
-    def iter_pid(ctl):
-        x = 0.98
-        while True:
-            x = ctl.update(0.5,x)
-            yield np.copy(0.5-x)
-    def tot_err(ctl,n):
-        return 1e2*sum([np.abs(next(iter_pid(ctl))) for i in range(n)])
-    def cost(x):
-        out = tot_err(PID(mdl.patient,mdl,x[0],x[1],x[2],0.5),50)
-        c = PID(mdl.patient,mdl,x[0],x[1],x[2],0.5)
-        print(x,out,[next(iter_pid(c)) for i in range(50)])
-        return out
-    xs = np.linspace(-3,3,10)
-    ys = np.linspace(-3,3,10)
-    X,Y = np.meshgrid(xs,ys)
-    zs = np.array([[cost((x,0,y)) for x in xs] for y in ys])
-    print(zs)
-    return xs,ys,zs
+        tf.print(y)
+        return tf.vectorized_map(lambda x: pid_loss(x[0],x[1],x[2],1,tf.constant(0.5,tf.float32),140,pred),tf.cast(y,tf.float32))
+    z = tf.TensorArray(tf.float32,size=100)
+    for j in range(p.shape[0]):
+        z = z.write(j,cost(tf.stack((P[j],tf.tile(tf.constant([i],tf.float32),P[j].shape),D[j]),1)))
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    ax.plot_surface(P,D,z.stack())
 
 def test():
     p = Patient(36,160,60,Gender.F)
     #n = Pharmacodynamic(p,2.0321,2.3266,13.9395,26.6474) 
     n = NNET(p)
+    #pid_loss_plot(n,0.0)
     refs = (tf.ones(15)*0.5)
 
     c = MPC(p,n.mdl,5)
@@ -242,10 +217,11 @@ def test():
     #fig,ax = plt.subplots(subplot_kw=dict(projection='3d'))
     #ax.plot_surface(*PID_plot(n))
     #optimized_values = swarm_PID(n,0.5)
-    #optimized_values =get_PID(n,0.5)
+    #optimized_values =get_PID(n,0.5,swarm_PID(n,0.5))
+    #print(time.time()-st)
     #optimized_values =[-10,np.inf,0]
     #c = PID(n,*optimized_values)
-    #c = PID(p,n,-1.55470255,0, -0.00467335)
+    #c = PID(n,-3,-0.003,3)
     #print(time.time()-st,c.k)
     bis = [n(tf.ones([1,180,1])*1e-16,tf.ones([1,180,1])*1e-16)]*50
     #print(bis)
@@ -256,7 +232,6 @@ def test():
         for i in range(10):
             st = time.time()
             b = c.update(r,bis[-1])
-            print(b,c.prop[0,-1,0],c.remi[0,-1,0])
             #bis.extend(b+(int(a>len(refs[int(len(refs)/2):])/2))*0.2+np.random.normal(scale=0.05))
             bis.append(b)
             rs.extend([r])
